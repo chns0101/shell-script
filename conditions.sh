27e57f1db31afda08da84d817cd0212f67b545b0
chns0101 <chns0101@gmail.com>
2024-04-22 11:25:25 +0530

    conditions test

--- conditions.sh
+++ conditions.sh
@@ -6,11 +6,23 @@ echo "addition of 2 numbers"
 
 NO1=$1
 NO2=$2
+NO3=$3
+
+#gt greater than ,lt lesser than,eq equal, nq not equal to
+if [ $NO3 -gt 10 ]
+then
+    echo "given number $NO3 is greater than 10"
+else
+    echo "given number $NO3 is lesser than 10"
+fi
 
 SUM=$(($NO1+$NO2))
 
-if [ $NO1 > 0 && $NO2 > 0]{
-echo "total of $NO1 and $NO2 is :$SUM"
-}
+if [ $NO1 -gt 0 && $NO2 -gt 0]
+then
+    echo "total of $NO1 and $NO2 is :$SUM"
 else
     echo "please enter number"
+fi
+
+
