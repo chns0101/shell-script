ae014f0d6f3445c4317e82dc6d9834820919a4af
chns0101 <chns0101@gmail.com>
2024-04-22 09:40:09 +0530

    variables test

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh :: Hello suresh how are you"
+echo "Suresh :: Hi Ramesh ,Im fine. How are you"
+echo "Ramesh :: Im fine too. How is your work"
+echo "Suresh :: not bad. I am thinking to upgrade todevops"
+
