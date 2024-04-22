c12062f565d5c82a06f2953ab79e26f5b68f4a6a
chns0101 <chns0101@gmail.com>
2024-04-22 10:28:17 +0530

    arguments test

--- variables.sh
+++ variables.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-echo "Ramesh :: Hello suresh how are you"
-echo "Suresh :: Hi Ramesh ,Im fine. How are you"
-echo "Ramesh :: Im fine too. How is your work"
-echo "Suresh :: not bad. I am thinking to upgrade todevops"
+#declaring the variables
+PERSON1:Ramesh
+PERSON2:Suresh
+
+#referring the variables
+echo "$PERSON1 :: Hello $PERSON2 how are you"
+echo "$PERSON2 :: Hi $PERSON1 ,Im fine. How are you"
+echo "$PERSON1 :: Im fine too. How is your work"
+echo "$PERSON2 :: not bad. I am thinking to upgrade todevops"
+
+#arguments or parameters
+
+P1=$1
+P2=$2
+
+echo "$P1 :: Hello $P2 how are you"
+echo "$P2 :: Hi $P1 ,Im fine. How are you"
+echo "$P1 :: Im fine too. How is your work"
+echo "$P2 :: not bad. I am thinking to upgrade todevops"
 
