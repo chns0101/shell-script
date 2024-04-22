74c023f315b133a231c7a99b7d41d2bfe50a3220
chns0101 <chns0101@gmail.com>
2024-04-22 11:00:26 +0530

    datatypes test

--- data-types.sh
+++ data-types.sh
@@ -23,8 +23,9 @@ NO2=$2
 
 SUM=$(($NO1+$NO2))
 
-if [ $NO1 > 0 && $NO2 > 0]
-    echo "total of $NO1 and $NO2 is :$SUM"
+if [ $NO1 > 0 && $NO2 > 0]{
+echo "total of $NO1 and $NO2 is :$SUM"
+}
 else
     echo "please enter number"
 
