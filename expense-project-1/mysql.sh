278ac72e0da3aac344958326c35a47c72d8f1a22
chns0101 <chns0101@gmail.com>
2024-04-29 08:40:15 +0530

    scrip test

--- expense-project-1/mysql.sh
+++ expense-project-1/mysql.sh
@@ -8,18 +8,10 @@ echo "please enter DB password"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing mysql-server"
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling mysql-server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting mysql-server"
-
-#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-#VALIDATE $? "setting up root password"
-
-#below code will be useful for idempotent nature
 
 mysql -h db.daws78.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
@@ -29,12 +21,3 @@ then
 else
     echo -e "MySQL Root password is already setup... $Y SKIPPING $N"
 fi
-
-
-
-
-#create instance
-#login into mysql instance
-#git clone code
-#sudo sh mysql.sh
-#netstat -lntp  port 3306
